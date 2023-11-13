c3577d07ff4a2f31a3f579537193fd758ed7be2e
BrandonERivera <brandonellis.rivera@gmail.com>
2023-11-13 17:29:46 -0500

    did styling now for heroku and finished

--- controllers/api/blogpostRoutes.js
+++ controllers/api/blogpostRoutes.js
@@ -9,6 +9,7 @@ router.post('/', withAuth, async (req, res) => {
 			user_id: req.session.user_id,
 		});
 		res.json(newBlogPost);
+		console.log(newBlogPost);
 	} catch (err) {
 		res.status(500).json(err);
 	}
